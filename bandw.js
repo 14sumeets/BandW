8fbf0375afa5b22b532d8ea582c30ce65e734b9f
Sumeet Sharma <sumeet.sharma@berkeley.edu>
2012-12-20 02:36:36 -0600

    user set password upon logging in right after registration works

--- bandw.js
+++ bandw.js
@@ -5,6 +5,38 @@ if (Meteor.isClient) {
 	Template.page.isLoggedIn = function() {
 		return Meteor.user() != null
 	};
+	
+	Template.newlyregistered.events({
+		'click button': function () {
+			var pw = $('#pw1')[0].value
+			if (  pw == $('#pw2')[0].value ) {
+				$('#setpwerror').text("")
+				Meteor.call('resetPw',pw,function(error,result) {
+					if (result == "success") {
+						$("#setpwerror").text("Success!")
+						$("#newlyregistered").animate({height: 0, opacity: 0}, 'slow', function() {
+						        $(this).remove();
+						    });
+					} else {
+						$("#setpwerror").text(result)
+					}
+				});
+			} else {
+				$('#setpwerror').text("Error: Passwords entered do not match.")
+			}
+		}
+	});
+	
+	Template.userhomeheader.events({
+		'click button': function () {
+			console.log("LOG ME OUT!")
+			Meteor.logout();
+		}
+	});
+	Template.rightofmenu.needtomakepw = function() {
+		return Meteor.user().profile != undefined && Meteor.user().profile.makepw == true;
+	}
+	
 	Template.signup_form.events({
 		'click button#createUser': function () {
 			var username = $('#newusername')[0].value;
@@ -13,7 +45,8 @@ if (Meteor.isClient) {
 			console.log(pw)
 			var options = {
 				username: username,
-				password: pw
+				password: pw,
+				profile: {makepw: true}
 			};
 			Accounts.createUser(options,function (err) {
 				console.log("Creating user...");
@@ -46,5 +79,22 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+	Meteor.methods({
+		resetPw: function(pw) {
+			if (Meteor.user() != null) {
+				Accounts.setPassword(Meteor.user()._id,pw);
+				Meteor.users.update({_id:Meteor.user()._id},{$set: {profile : {makepw:false}}}  )
+				console.log("Password reset!")
+				return "success";
+			}
+			return "Error: Not logged in";	
+		}
+	});
+
+
+
+	//Meteor.users.remove({});
+	//Meteor.users.find().forEach(function(o){ console.log(o)});
+	//console.log("DONE")
   });
 }
